2a842b507fc96da6a5cf44c695e81c46695c92a7
Diana725 <wachira.wangari2019@students.jkuat.ac.ke>
2024-11-22 12:50:47 +0300

    Updated buyer email verification frontend url

--- ecomm-backend1/app/Mail/PasswordResetMail.php
+++ ecomm-backend1/app/Mail/PasswordResetMail.php
@@ -16,7 +16,8 @@ class PasswordResetMail extends Mailable
     {
         // Modify the reset link to include both email and token
         // Make sure FRONTEND_URL is set in .env, or use a full URL like 'http://localhost:3000'
-        $this->resetLink = rtrim(env('FRONTEND_URL'), '/') . '/password/reset-form?token=' . $token . '&email=' . urlencode($email);
+        $frontendUrl = 'https://www.buyer.maizeai.me';
+        $this->resetLink = rtrim(env($frontendUrl), '/') . '/password/reset-form?token=' . $token . '&email=' . urlencode($email);
     }
 
     public function build()
